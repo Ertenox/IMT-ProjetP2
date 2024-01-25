0c939387c7d0637f4577827b5fc8bf0b56552919
Phoquiche <dustybertin@gmail.com>
2024-01-25 13:18:19 +0100

    Retirer les bêtises écrites en test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("BERTIN ANTOINE | NOA PAILLETTE | IMT NORD EUROPE | FISA TI 2026");
-        System.out.println("Ca me fane!");
-        System.out.println("Ca me fane de ouf!");
     }
 }
