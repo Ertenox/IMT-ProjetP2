d3a3e48e54b19d6ad8dfab81c3299673c9137ff4
Phoquiche <dustybertin@gmail.com>
2024-01-25 13:11:25 +0100

    Ajout nom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("BERTIN ANTOINE | NOA PAILLETTE | IMT NORD EUROPE | FISA TI 2026");
     }
 }
