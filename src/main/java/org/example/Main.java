18ff10739b8da0f0cb1707d6a744d98b5878b6cd
phoquiche <dustybertin@gmail.com>
2024-01-25 19:51:06 +0100

    Test sur PC Perso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("BERTIN ANTOINE | NOA PAILLETTE | IMT NORD EUROPE | FISA TI 2026");
-        System.out.print("Test sur PC");
+
     }
 }
